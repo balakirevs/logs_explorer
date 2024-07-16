18aa703b44da340fc75c1b792c3b778cb66181e7
Aleksandr Balakirev <oleksandr.balakiriev@salt.ch>
2024-07-16 08:44:21 +0200

    adopt for rails 6

--- app/controllers/logs_explorer/console_controller.rb
+++ app/controllers/logs_explorer/console_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
-Rails.application.class.parent_name.constantize::Application.load_tasks if defined?(Rails)
+parent_name_method = Rails.application.class.respond_to?(:module_parent_name) ? :module_parent_name : :parent_name
+Rails.application.class.send(parent_name_method).constantize::Application.load_tasks if defined?(Rails)
+
 require_relative './base_controller.rb'
 
 module LogsExplorer
